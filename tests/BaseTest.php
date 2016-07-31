76efcb9d82c27c7ea90ed7367f73fac5675f7c65
Cristian Tabacitu <cristitabacitu@gmail.com>
2016-07-31 12:06:26 +0300

    bogus unit tests

---
+++ tests/BaseTest.php
@@ -0,0 +1,14 @@
+<?php
+
+class BaseTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+}
