ed95f8fee8017ccf1a37d12a97dbf455e749f9ac
reeslo <r33s@live.fr>
2016-10-20 12:08:41 +0200

    Update UserCrudController.php

--- src/app/Http/Controllers/UserCrudController.php
+++ src/app/Http/Controllers/UserCrudController.php
@@ -17,7 +17,7 @@ class UserCrudController extends CrudController
 
         $this->crud->setModel(config('backpack.permissionmanager.user_model'));
         $this->crud->setEntityNameStrings('user', 'users');
-        $this->crud->setRoute('admin/user');
+        $this->crud->setRoute(config('backpack.base.route_prefix').'/user');
         $this->crud->setColumns([
                 [
                     'name'  => 'name',
