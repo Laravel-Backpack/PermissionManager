726239b26f36318bc1fde4a14b50fb01f029a986
Atanas Atanasov <atanas.m.atanasov@gmail.com>
2016-10-23 11:51:22 +0300

    fixed admin prefix as others Backpack packages

--- src/PermissionManagerServiceProvider.php
+++ src/PermissionManagerServiceProvider.php
@@ -53,7 +53,7 @@ class PermissionManagerServiceProvider extends ServiceProvider
     public function setupRoutes(Router $router)
     {
         $router->group(['namespace' => 'Backpack\PermissionManager\app\Http\Controllers'], function ($router) {
-            \Route::group(['prefix' => config('backpack.base.route_prefix'), 'middleware' => ['web', 'admin']], function () {
+            \Route::group(['prefix' => config('backpack.base.route_prefix','admin'), 'middleware' => ['web', 'admin']], function () {
                 \CRUD::resource('permission', 'PermissionCrudController');
                 \CRUD::resource('role', 'RoleCrudController');
                 \CRUD::resource('user', 'UserCrudController');
