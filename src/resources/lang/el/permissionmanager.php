04377439f28f66398686d074079feb984ac91511
Alex <discodancer@gmail.com>
2017-02-12 16:46:16 +0200

    Added Greek translation

---
+++ src/resources/lang/el/permissionmanager.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Permission Manager Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used for Laravel Backpack - Permission Manager
+    |
+    */
+    'name'                  => 'Όνομα',
+    'role'                  => 'Ρόλος',
+    'roles'                 => 'Ρόλοι',
+    'roles_have_permission' => 'Ρόλοι με αυτό το δικαίωμα',
+    'permission_singular'   => 'δικαίωμα',
+    'permission_plural'     => 'Δικαιώματα',
+    'user_singular'         => 'Χρήστης',
+    'user_plural'           => 'Χρήστες',
+    'email'                 => 'Email',
+    'extra_permissions'     => 'Πρόσθετα δικαιώματα',
+    'password'              => 'Κωδικός',
+    'password_confirmation' => 'Επανάληψη κωδικού',
+    'user_role_permission'  => 'Ρόλοι και Δικαιώματα Χρήστη',
+    'user'                  => 'Χρήστης',
+    'users'                 => 'Χρήστες',
+
+];
