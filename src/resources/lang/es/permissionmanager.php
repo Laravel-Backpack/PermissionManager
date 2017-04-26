a99dae8f56a3abe5d079c351ead6681dc3d66952
Cesar Bretana Gonzalez <bretanac@gmail.com>
2017-04-26 18:25:19 -0400

    Spanish Language

---
+++ src/resources/lang/es/permissionmanager.php
@@ -0,0 +1,29 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Permission Manager Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used for Laravel Backpack - Permission Manager
+    | Author: Lúdio Oliveira <ludio.ao@gmail.com>
+    |
+    */
+    'name'                  => 'Nombre',
+    'role'                  => 'Role',
+    'roles'                 => 'Roles',
+    'roles_have_permission' => 'Roles que contienen este permiso',
+    'permission_singular'   => 'permiso',
+    'permission_plural'     => 'permisos',
+    'user_singular'         => 'Usuario',
+    'user_plural'           => 'Usuarios',
+    'email'                 => 'Correo Electrónico',
+    'extra_permissions'     => 'Permisos Extra',
+    'password'              => 'Contraseña',
+    'password_confirmation' => 'Confirmación de contraseña',
+    'user_role_permission'  => 'Permiso de Roles de Usuario',
+    'user'                  => 'Usuario',
+    'users'                 => 'Usuarios',
+
+];
