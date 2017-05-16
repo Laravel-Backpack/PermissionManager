6ca60e35614508dd5266e341edaf974d02a35d03
Jelmer Visser <j.visser@jvis.nl>
2017-05-16 10:45:51 +0200

    Dutch translation

---
+++ src/resources/lang/nl/permissionmanager.php
@@ -0,0 +1,29 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Permission Manager Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used for Laravel Backpack - Permission Manager
+    | Author: Lúdio Oliveira <ludio.ao@gmail.com>
+    |
+    */
+    'name'                  => 'Naam',
+    'role'                  => 'Rol',
+    'roles'                 => 'Rollen',
+    'roles_have_permission' => 'Rollen die deze permissie hebben',
+    'permission_singular'   => 'Permissie',
+    'permission_plural'     => 'Permissies',
+    'user_singular'         => 'Gebruiker',
+    'user_plural'           => 'Gebruikers',
+    'email'                 => 'E-mail',
+    'extra_permissions'     => 'Extra permissies',
+    'password'              => 'Wachtwoord',
+    'password_confirmation' => 'Wachtwoord bevestigen',
+    'user_role_permission'  => 'Rollen en permissies voor gebruiker',
+    'user'                  => 'Gebruiker',
+    'users'                 => 'Gebruikers',
+
+];
