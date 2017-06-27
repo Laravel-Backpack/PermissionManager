9b0b37706558f5c45258f865dd289881d717fb32
Toni Almeida <promatik@gmail.com>
2017-06-27 09:46:02 +0100

    Portuguese translations

---
+++ src/resources/lang/pt/permissionmanager.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Permission Manager Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used for Laravel Backpack - Permission Manager
+    |
+    */
+    'name'                  => 'Nome',
+    'role'                  => 'Cargo',
+    'roles'                 => 'Cargos',
+    'roles_have_permission' => 'Cargos com esta permissão',
+    'permission_singular'   => 'permissão',
+    'permission_plural'     => 'permissões',
+    'user_singular'         => 'Utilizador',
+    'user_plural'           => 'Utilizadores',
+    'email'                 => 'Email',
+    'extra_permissions'     => 'Permissões extra',
+    'password'              => 'Palavra-passe',
+    'password_confirmation' => 'Confirmar palavra-passe',
+    'user_role_permission'  => 'Cargo e permissões do utilizador',
+    'user'                  => 'Utilizador',
+    'users'                 => 'Utilizadores',
+
+];
