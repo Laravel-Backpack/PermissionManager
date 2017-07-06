2eaa5d9bf6569989016d7e9ea4225f2c2778d3c8
Cristian Tabacitu <cristitabacitu@gmail.com>
2017-07-06 07:39:22 +0300

    Update PermissionManagerServiceProvider.php

--- src/PermissionManagerServiceProvider.php
+++ src/PermissionManagerServiceProvider.php
@@ -19,7 +19,7 @@ class PermissionManagerServiceProvider extends ServiceProvider
     /**
      * Where the route file lives, both inside the package and in the app (if overwritten).
      *
-     * @var bool
+     * @var string
      */
     public $routeFilePath = '/routes/backpack/permissionmanager.php';
 
