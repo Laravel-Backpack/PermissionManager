f68e11bd92fb15587ef8e650547b971cb4cde245
Nikita <exotickg1@gmail.com>
2017-08-07 23:19:51 +0600

    Translating to Russian language
    Some strings for Russian language

---
+++ src/resources/lang/ru/permissionmanager.php
@@ -0,0 +1,27 @@
+<?php
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Permission Manager Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used for Laravel Backpack - Permission Manager
+    | Author: Lúdio Oliveira <ludio.ao@gmail.com>, translating to Russian: Nikita K. <exotickg1@gmail.com>
+    |
+    */
+    'name'                  => 'Имя',
+    'role'                  => 'Роль',
+    'roles'                 => 'Роли',
+    'roles_have_permission' => 'Роли, которые имеют это разрешение',
+    'permission_singular'   => 'разрешение',
+    'permission_plural'     => 'разрешения',
+    'user_singular'         => 'Пользователь',
+    'user_plural'           => 'Пользователи',
+    'email'                 => 'Почта',
+    'extra_permissions'     => 'Дополнительные разрешения',
+    'password'              => 'Пароль',
+    'password_confirmation' => 'Повторите пароль',
+    'user_role_permission'  => 'Разрешения роли пользователя',
+    'user'                  => 'Пользователь',
+    'users'                 => 'Пользователи',
+];
