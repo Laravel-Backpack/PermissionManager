7a6f2f53411014e5e9f423d84c3f4e796cbd5697
Julien Cauvin <contact@7ute.fr>
2017-12-22 10:48:58 +0100

    Added french translation

---
+++ src/resources/lang/fr/permissionmanager.php
@@ -0,0 +1,29 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Permission Manager Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used for Laravel Backpack - Permission Manager
+    | Author: Julien Cauvin <contact@7ute.fr>
+    |
+    */
+    'name'                  => 'Nom',
+    'role'                  => 'Rôle',
+    'roles'                 => 'Rôles',
+    'roles_have_permission' => 'Rôles avec cette permission',
+    'permission_singular'   => 'permission',
+    'permission_plural'     => 'permissions',
+    'user_singular'         => 'Utilisateur',
+    'user_plural'           => 'Utilisateurs',
+    'email'                 => 'Email',
+    'extra_permissions'     => 'Permissions supplémentaires',
+    'password'              => 'Mot de passe',
+    'password_confirmation' => 'Confirmation du mot de passe',
+    'user_role_permission'  => 'Rôles et permissions d’utilisateur',
+    'user'                  => 'Utilisateur',
+    'users'                 => 'Utilisateurs',
+
+];
