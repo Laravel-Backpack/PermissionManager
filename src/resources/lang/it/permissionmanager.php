43fa6154f327b0ed7563b123d3cf80f441e662ff
Roberto Butti <roberto.butti@gmail.com>
2018-06-17 09:48:42 +0200

    Added Italian translation

---
+++ src/resources/lang/it/permissionmanager.php
@@ -0,0 +1,30 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Permission Manager Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used for Laravel Backpack - Permission Manager
+    | Author: Roberto Butti ( https://github.com/roberto-butti )
+    | Language: Italian
+    |
+    */
+    'name'                  => 'Nome',
+    'role'                  => 'Ruolo',
+    'roles'                 => 'Ruoli',
+    'roles_have_permission' => 'Ruoli con questo permesso',
+    'permission_singular'   => 'Permesso',
+    'permission_plural'     => 'Permessi',
+    'user_singular'         => 'Utente',
+    'user_plural'           => 'Utenti',
+    'email'                 => 'Email',
+    'extra_permissions'     => 'Permessi Extra',
+    'password'              => 'Password',
+    'password_confirmation' => 'Conferma Password',
+    'user_role_permission'  => 'Utenti Ruoli Permessi',
+    'user'                  => 'Utente',
+    'users'                 => 'Utenti',
+
+];
