b86fa8b75d748ab56e7fcaed42bbf857175253b3
Cristian Tabacitu <cristitabacitu@gmail.com>
2018-06-21 06:40:29 +0000

    Apply fixes from StyleCI

--- src/app/Http/Requests/PermissionCrudRequest.php
+++ src/app/Http/Requests/PermissionCrudRequest.php
@@ -14,7 +14,7 @@ class PermissionCrudRequest extends FormRequest
     public function authorize()
     {
         // only allow updates if the user is logged in
-	    return backpack_auth()->check();
+        return backpack_auth()->check();
     }
 
     /**
--- src/app/Http/Requests/UserUpdateCrudRequest.php
+++ src/app/Http/Requests/UserUpdateCrudRequest.php
@@ -6,16 +6,16 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class UserUpdateCrudRequest extends FormRequest
 {
-	/**
-	 * Determine if the user is authorized to make this request.
-	 *
-	 * @return bool
-	 */
-	public function authorize()
-	{
-		// only allow updates if the user is logged in
-		return backpack_auth()->check();
-	}
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        // only allow updates if the user is logged in
+        return backpack_auth()->check();
+    }
 
     /**
      * Get the validation rules that apply to the request.
