0064b218c94e1fda302b94f7661ccd91b8571211
Azaric, Stefan <stefan.azaric@zuehlke.com>
2019-01-22 20:29:27 +0100

    added serbian translation

---
+++ src/resources/lang/sr/permissionmanager.php
@@ -0,0 +1,30 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Permission Manager Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used for Laravel Backpack - Permission Manager
+    | Author: Lúdio Oliveira <ludio.ao@gmail.com>
+    |
+    */
+    'name'                  => 'Ime',
+    'role'                  => 'Uloga',
+    'roles'                 => 'Uloge',
+    'roles_have_permission' => 'Uloge koje imaju dozvolu',
+    'permission_singular'   => 'Dozvola',
+    'permission_plural'     => 'Dozvole',
+    'user_singular'         => 'Korisnik',
+    'user_plural'           => 'Korisnici',
+    'email'                 => 'Email',
+    'extra_permissions'     => 'Dodatne dozvole',
+    'password'              => 'Šifra',
+    'password_confirmation' => 'Potvrdna šifra',
+    'user_role_permission'  => 'Korisnik uloga Dozvole',
+    'user'                  => 'Korisnik',
+    'users'                 => 'Korisnici',
+    'guard_type'            => 'Tip garda',
+
+];
