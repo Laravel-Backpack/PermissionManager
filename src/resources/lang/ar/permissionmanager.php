317d622d74ffd78e521ad9a8f559c04a5e23a30e
Ahmed Barabbud <a.a.barbod@gmail.com>
2019-10-10 09:52:44 +0300

    Add Arabic language.

---
+++ src/resources/lang/ar/permissionmanager.php
@@ -0,0 +1,30 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Permission Manager Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used for Laravel Backpack - Permission Manager
+    | Author: ِAhmed Barabbud <a.a.barbod@gmail.com>
+    |
+    */
+    'name'                  => 'اسم',
+    'role'                  => 'وظيفة',
+    'roles'                 => 'وظائف',
+    'roles_have_permission' => 'الوظائف التي لديها هذه الصلاحية',
+    'permission_singular'   => 'الصلاحية',
+    'permission_plural'     => 'الصلاحيات',
+    'user_singular'         => 'المستخدم',
+    'user_plural'           => 'المستخدمين',
+    'email'                 => 'البريد الالكتروني',
+    'extra_permissions'     => 'الصلاحيات الاضافية',
+    'password'              => 'كلمة المرور',
+    'password_confirmation' => 'تأكيد كلمة المرور',
+    'user_role_permission'  => 'صلاحيات دور المستخدم',
+    'user'                  => 'المستخدم',
+    'users'                 => 'المستخدمين',
+    'guard_type'            => 'نوع الحماية',
+
+];
