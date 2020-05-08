a582caab4858a8d43f896719284ab67d19d7d1a3
Cristian Tabacitu <cristitabacitu@gmail.com>
2020-05-08 09:01:19 +0000

    Apply fixes from StyleCI

--- src/app/Http/Controllers/UserCrudController.php
+++ src/app/Http/Controllers/UserCrudController.php
@@ -61,10 +61,10 @@ class UserCrudController extends CrudController
             ],
             config('permission.models.role')::all()->pluck('name', 'id')->toArray(),
             function ($value) { // if the filter is active
-            $this->crud->addClause('whereHas', 'roles', function ($query) use ($value) {
-                $query->where('role_id', '=', $value);
-            });
-        }
+                $this->crud->addClause('whereHas', 'roles', function ($query) use ($value) {
+                    $query->where('role_id', '=', $value);
+                });
+            }
         );
 
         // Extra Permission Filter
@@ -76,10 +76,10 @@ class UserCrudController extends CrudController
             ],
             config('permission.models.permission')::all()->pluck('name', 'id')->toArray(),
             function ($value) { // if the filter is active
-            $this->crud->addClause('whereHas', 'permissions', function ($query) use ($value) {
-                $query->where('permission_id', '=', $value);
-            });
-        }
+                $this->crud->addClause('whereHas', 'permissions', function ($query) use ($value) {
+                    $query->where('permission_id', '=', $value);
+                });
+            }
         );
     }
 
