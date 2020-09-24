c7edaf24fd82d585e77828e78e31df24b0fc485d
Selçuk Çukur <selcukcukur@outlook.com.tr>
2020-09-24 20:40:09 +0300

    Turkish language file is included.

---
+++ src/resources/lang/tr/permissionmanager.php
@@ -0,0 +1,29 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Permission Manager Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used for Laravel Backpack - Permission Manager
+    | Author: Lúdio Oliveira <ludio.ao@gmail.com>, translatin to Turkish : Selçuk Çukur <hk@selcukcukur.com.tr>
+    |
+    */
+    'name'                  => 'Adı',
+    'role'                  => 'Yetki',
+    'roles'                 => 'Yetkiler',
+    'roles_have_permission' => 'Bu izne sahip yetkiler',
+    'permission_singular'   => 'izin',
+    'permission_plural'     => 'izinler',
+    'user_singular'         => 'Kullanıcı',
+    'user_plural'           => 'Kullanıcılar',
+    'email'                 => 'Eposta',
+    'extra_permissions'     => 'Ekstra İzinler',
+    'password'              => 'Parola',
+    'password_confirmation' => 'Parola (Tekrar)',
+    'user_role_permission'  => 'Kullanıcı Yetki İzinleri',
+    'user'                  => 'Kullanıcı',
+    'users'                 => 'Kullanıcılar',
+    'guard_type'            => 'Güvenlik Tipi',
+];
