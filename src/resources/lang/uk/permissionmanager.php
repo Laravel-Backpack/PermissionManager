60d5aa540abff993c1fc34f055b767d5d37ece19
Pavel Tkachenko <pavel@kangaroopartners.com>
2021-04-12 18:30:36 +0300

    Added Ukrainian translation.

---
+++ src/resources/lang/uk/permissionmanager.php
@@ -0,0 +1,30 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Permission Manager Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used for Laravel Backpack - Permission Manager
+    | Author: Lúdio Oliveira <ludio.ao@gmail.com>, translated to Ukrainian: Pavel Tkachenko <kazantip@gmail.com>
+    |
+    */
+    'name'                  => "Ім'я",
+    'role'                  => 'Роль',
+    'roles'                 => 'Ролі',
+    'roles_have_permission' => 'Ролі, які мають цей дозвіл',
+    'permission_singular'   => 'дозвіл',
+    'permission_plural'     => 'дозволи',
+    'user_singular'         => 'Користувач',
+    'user_plural'           => 'Користувачі',
+    'email'                 => 'Електронна пошта',
+    'extra_permissions'     => 'Додаткові дозволи',
+    'password'              => 'Пароль',
+    'password_confirmation' => 'Підтвердження паролю',
+    'user_role_permission'  => 'Дозволи ролі користувача',
+    'user'                  => 'Користувач',
+    'users'                 => 'Користувачі',
+    'guard_type'            => 'Тип охорони',
+
+];
