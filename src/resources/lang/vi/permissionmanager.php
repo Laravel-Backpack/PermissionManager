6d4288aced041f50f286998c701a3c8f2e82b1ac
Sang Lu <connect.with.sang@gmail.com>
2022-02-07 17:51:11 +0700

    Add Vietnamese translation

---
+++ src/resources/lang/vi/permissionmanager.php
@@ -0,0 +1,30 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Permission Manager Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used for Laravel Backpack - Permission Manager
+    | Author: Sang Lu <connect.with.sang@gmail.com>
+    |
+    */
+    'name'                  => 'Tên',
+    'role'                  => 'Vai trò',
+    'roles'                 => 'Các vai trò',
+    'roles_have_permission' => 'Các vai trò được cấp quyền này',
+    'permission_singular'   => 'Quyền',
+    'permission_plural'     => 'Các quyền',
+    'user_singular'         => 'Người dùng',
+    'user_plural'           => 'Các người dùng',
+    'email'                 => 'Email',
+    'extra_permissions'     => 'Các quyền bổ sung',
+    'password'              => 'Mật khẩu',
+    'password_confirmation' => 'Xác nhận Mật khẩu',
+    'user_role_permission'  => 'Người dùng, Vai trò, Các quyền',
+    'user'                  => 'Người dùng',
+    'users'                 => 'Các người dùng',
+    'guard_type'            => 'Lớp bảo vệ',
+
+];
